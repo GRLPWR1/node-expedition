56de9b42b4835a3770a9c5adfc1a8917ed2d966d
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-14 21:27:28 +0300

    read the txt files but still can't pass the test

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,57 +1,82 @@
+const fs = require('fs');
 
+const crew = fs.readFileSync('./src/crew.txt', 'utf-8').split('\r\n');
+console.log(crew);
+const rovers = fs.readFileSync('./src/equipment.txt', 'utf-8').split('\r\n');
+console.log(rovers);
+const rockets = fs.readFileSync('./src/rockets.txt', 'utf-8').split('\r\n');
+console.log(rockets);
 
 //позволяет выбрать самого опытного капитана
-function getRightCaptain(arr) {
-  let candidates = arr.filter(el => el.includes('Капитан'));
-  console.log(candidates);
+function getRightCaptain() {
+  let candidates = crew.filter(el => el.includes('Капитан'));
   let experience = 0;
   let mostExperienced;
     for (let el of candidates) {
       if (el.substr(-2, 2) > experience) {
         experience = +el.substr(-2, 2);
-        console.log(experience);
         mostExperienced = el;
       }
     }
     return mostExperienced;
   }
-
-
-console.log(getRightCaptain([
-  'Роберт Стивенсон, м, Капитан, 12',
-  'Кэтерин Лоу, ж, Врач, 9',
-  'Уильям Блейк, м, Бортмеханик, 11',
-  'Стив Джонсон, м, Капитан, 23',
-  'Клара Томпсон, ж, Врач, 10',
-  'Том Браун, м, Врач, 14',
-  'Джуди Лестер, ж, Бортмеханик, 16',
-]))
+console.log('Cамый опытный капитан: ' + getRightCaptain());
 
 //позволяет выбрать самого опытного врача среди женщин
 function getRightDoc() {
-
+  let candidates = crew.filter(el => (el.includes('Врач') && el.includes('ж')));
+  let experience = 0;
+  let mostExperienced;
+  for (let el of candidates){
+    if (el.substr(-2,2) > experience){
+      experience = +el.substr(-2, 2);
+      mostExperienced = el;
+    }
+  }
+  return mostExperienced;
 }
+console.log('Cамый опытный врач-женщина: '+ getRightDoc())
 
 //позволяет выбрать всех бортмехаников
 function getAllEngineers() {
-
+return crew.filter(el => el.includes('Бортмеханик'));
 }
+console.log('Бортмеханики: ' + getAllEngineers())
 
 //Позволяет отобрать все марсоходы
 function getAllRovers(){
-
+  return rovers.filter(el => el.includes('марсоход'));
 }
+console.log('Марсоходы: ' + getAllRovers())
 
 //позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
 function getRightRovers(){
-
+  let rovers = rovers.filter(el => el.includes('марсоход'));
+  let rightRovers = [];
+  for (let el of rovers) {
+    if (+el.substr(-1, 2) > 3) {
+      rightRovers.push(el);
+    }
+  }
+  return rightRovers;
 }
+console.log('Марсоходы, которые смогут прослужить больше 3 лет: ' + getRightRovers())
 
 //позволяет выбрать ракету с максимальной дальностью полёта
 function getRightRocket(){
-
+  let newArr = rockets.map(el => el.split(','));
+  console.log(newArr);
+  let flightRange = 0;
+  let rightRocket;
+  for (el of newArr) {
+      if (+el[2] > flightRange) {
+        flightRange = +el[2]
+        rightRocket = el;
+      }
+  }
+  return rightRocket;
 }
-
+console.log('Ракета с максимальной дальностью полёта: ' + getRightRocket())
 
 module.exports = {
   getRightCaptain,
